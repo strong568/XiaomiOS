99d9047b5baf58f5c37a13b0f56a6a889afad2fa
trkai <161140493+trkai@users.noreply.github.com>
2026-09-22 16:20:49 +0700

    Update update.sh

--- bin/package/COREPATCH/update.sh
+++ bin/package/COREPATCH/update.sh
@@ -13,4 +13,6 @@ elif [[ $AndroidVER == "15" ]];then
     bash $work_dir/bin/package/COREPATCH/jar_patcher_a15.sh
 elif [[ $AndroidVER == "16" ]];then
     bash $work_dir/bin/package/COREPATCH/jar_patcher_a16.sh
+elif [[ $AndroidVER == "17" ]];then
+    bash $work_dir/bin/package/COREPATCH/jar_patcher_a17.sh
 fi
