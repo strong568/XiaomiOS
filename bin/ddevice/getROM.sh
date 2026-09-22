871d213c7bdc64ad132e2a3623cf2fe1bce30d9b
trkai <161140493+trkai@users.noreply.github.com>
2026-09-23 01:09:08 +0700

    Update getROM.sh

--- bin/ddevice/getROM.sh
+++ bin/ddevice/getROM.sh
@@ -85,6 +85,10 @@ elif echo "$base_rom_code" | grep -q "OS2"; then
     ROM_OS="OS2"
 elif echo "$base_rom_code" | grep -q "OS3"; then
     ROM_OS="OS3"
+elif echo "$base_rom_code" | grep -q "OS4"; then
+    ROM_OS="OS4"
+elif echo "$base_rom_code" | grep -q "OS5"; then
+    ROM_OS="OS5"
 elif echo "$base_rom_code" | grep -q "V14"; then
     ROM_OS="MIUI"
 elif echo "$base_rom_code" | grep -q "V13"; then
