9fb237226c20c258284f18bb0a671c8ccb4788d8
trkai <161140493+trkai@users.noreply.github.com>
2026-09-23 14:26:26 +0700

    Enable Core Patch and Kousei Patcher scripts

--- bin/package/patchpackage.sh
+++ bin/package/patchpackage.sh
@@ -5,10 +5,9 @@ mods "Add Package..."
 target_dir="$work_dir/bin/package/"
 
 # Đã vô hiệu hóa Core Patch trên tất cả các phiên bản Android
-# bash $target_dir/COREPATCH/update.sh
-
+bash $target_dir/COREPATCH/update.sh
 bash $target_dir/DISABLE_AVB/DISABLEavb.sh
-# bash $target_dir/KouseiPatcher/update.sh
+bash $target_dir/KouseiPatcher/update.sh
 bash $target_dir/NOTIFICATION_FIX/notificationFIX.sh
 bash $target_dir/RefreshRate/1hz.sh
 
