62cea319007d4f688570065d4470c7e23f554f14
trkai <161140493+trkai@users.noreply.github.com>
2026-09-23 13:26:27 +0700

    Disable Core Patch update script
    Comment out the Core Patch update script execution.

--- bin/package/patchpackage.sh
+++ bin/package/patchpackage.sh
@@ -4,9 +4,12 @@ source $work_dir/functions.sh
 mods "Add Package..."
 target_dir="$work_dir/bin/package/"
 
-bash $target_dir/COREPATCH/update.sh
+# Đã vô hiệu hóa Core Patch trên tất cả các phiên bản Android
+# bash $target_dir/COREPATCH/update.sh
+
 bash $target_dir/DISABLE_AVB/DISABLEavb.sh
 bash $target_dir/KouseiPatcher/update.sh
 bash $target_dir/NOTIFICATION_FIX/notificationFIX.sh
 bash $target_dir/RefreshRate/1hz.sh
+
 mods "Add Package Done"
